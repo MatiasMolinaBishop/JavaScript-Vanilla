dc5d12860285553189b49876b3baa035a2cb5cbd
Matias Molina Bishop <molibi01@gmail.com>
2023-09-12 21:08:42 +0200

    added practice

--- script.js
+++ script.js
@@ -405,3 +405,30 @@ document.querySelector("button").addEventListener("click", () => {
 
   //console.log(rows);
 });
+
+///////////////////////////////////////
+// String Methods Practice
+
+const flights =
+  "_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30";
+
+// 🔴 Delayed Departure from FAO to TXL (11h25)
+//              Arrival from BRU to FAO (11h45)
+//   🔴 Delayed Arrival from HEL to FAO (12h05)
+//            Departure from FAO to LIS (12h30)
+
+for (const flight of flights.split("+")) {
+  //console.log(flight);
+  const [bound, from, to, time] = flight.split(";");
+  const output = `${
+    bound.startsWith("_Delayed") ? "👽" : ""
+  } ${bound.replaceAll("_", " ")}from ${
+    from[0].toLocaleUpperCase() +
+    from[1].toLocaleUpperCase() +
+    from[2].toLocaleUpperCase()
+  } to ${to.slice(0, 3).toLocaleUpperCase()} (${time.replace(
+    ":",
+    "h"
+  )})`.padStart(36);
+  console.log(output);
+}
